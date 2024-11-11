1407e15c926735ea58f99dc174873b385701c27f
Alisson Youssf <youssfbr@gmail.com>
2024-11-10 23:39:45 -0300

    Menu

--- src/main/java/com/github/youssfbr/screensound/principal/Principal.java
+++ src/main/java/com/github/youssfbr/screensound/principal/Principal.java
@@ -1,10 +1,53 @@
 package com.github.youssfbr.screensound.principal;
 
+import com.github.youssfbr.screensound.utils.Mensagens;
 import org.springframework.stereotype.Service;
+
+import java.util.Scanner;
+
 
 @Service
 public class Principal {
+
+    private final Scanner sc = new Scanner(System.in);
+
     public void exibeMenu() {
-        System.out.println("OK");
+        int opcao = -1;
+
+        while (opcao != 0) {
+            System.out.println(Mensagens.MENSAGENS_OPCOES);
+            opcao = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcao) {
+                case 0 -> System.out.println("Encerrando a aplicação...");
+                case 1 -> cadastrarArtistas();
+                case 2 -> cadastrarMusicas();
+                case 3 -> listarMusicas();
+                case 4 -> buscarMusicasPorArtista();
+                case 5 -> pesquisarDadosDoArtista();
+                default -> System.out.println("Opção inválida!");
+            }
+        }
+    }
+
+    private void pesquisarDadosDoArtista() {
+        System.out.println("Não implementado.");
+    }
+
+    private void buscarMusicasPorArtista() {
+        System.out.println("Não implementado.");
+    }
+
+    private void listarMusicas() {
+        System.out.println("Não implementado.");
+    }
+
+    private void cadastrarMusicas() {
+        System.out.println("Não implementado.");
+    }
+
+    private void cadastrarArtistas() {
+        System.out.println("Não implementado.");
     }
 }
---
+++ src/main/java/com/github/youssfbr/screensound/utils/Mensagens.java
@@ -0,0 +1,22 @@
+package com.github.youssfbr.screensound.utils;
+
+public class Mensagens {
+
+    private Mensagens() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static final String MENSAGENS_OPCOES = """
+                        
+                        *** Screen Sound Music ***
+                        
+                        1  - Cadastrar artistas
+                        2  - Cadastrar músicas
+                        3  - Listar músicas
+                        4  - Buscar músicas por artistas
+                        5  - Pesquisar dados sobre um artista
+                        -
+                        0 - Sair
+                        
+                        """;
+}
